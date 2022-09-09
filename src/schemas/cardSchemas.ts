67b605d1dd61d3a70c6153f682ea5ceada375b9e
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-09 20:41:45 -0300

    make schemas strict

--- src/schemas/cardSchemas.ts
+++ src/schemas/cardSchemas.ts
@@ -7,6 +7,6 @@ export const createSchema = joi.object({
     securityCode: joi.string().trim().pattern(/^[0-9]{3}$/).required(),
     expirationDate: joi.string().pattern(/^(0[1-9]|1[0-2])\/?([0-9]{2})$/).required(),
     password: joi.string().trim().required(),
-    isVirtual: joi.bool().required(),
+    isVirtual: joi.bool().required().strict(),
     type: joi.string().valid("credit", "debit", "both").required(),
 });
--- src/schemas/documentsSchemas.ts
+++ src/schemas/documentsSchemas.ts
@@ -4,7 +4,7 @@ export const documentBodySchema = joi.object({
     fullName: joi.string().trim().required(),
     issueDate: joi.string().trim().pattern(/^([0]?[1-9]|[1|2][0-9]|[3][0|1])[.\/-]([0]?[1-9]|[1][0-2])[.\/-]([0-9]{4}|[0-9]{2})$/).required(),
     expirationDate: joi.string().trim().pattern(/^([0]?[1-9]|[1|2][0-9]|[3][0|1])[.\/-]([0]?[1-9]|[1][0-2])[.\/-]([0-9]{4}|[0-9]{2})$/).required(),
-    registrationNumber: joi.number().required(),
+    registrationNumber: joi.number().required().strict(),
     issuingBody: joi.string().trim().required(),
     type: joi.string().valid("RG", "CNH").required(),
 });
