a666d87e91f086e89eef01d4481e956447a7d0d8
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-09 22:06:06 -0300

    fix auth service

--- src/services/authorizationServices.ts
+++ src/services/authorizationServices.ts
@@ -5,12 +5,12 @@ import * as passwordEncrypter from "../utils/passwordEncrypter";
 
 export async function signUp({email, password } : { email: string, password: string}) {
     const dbUsers = await usersRepository.findUsers();
-    if (dbUsers.some(user => user.email === email)) error.conflictError("email");
+    if (dbUsers.some(user => user.email === email)) throw error.conflictError("email");
 
     await usersRepository.insertUser({
         email,
         password: passwordEncrypter.hashPassword(password),
-    });
+    }); console.log("cheguei aqui")
 }
 
 export async function signIn({email, password } : { email: string, password: string}) {
